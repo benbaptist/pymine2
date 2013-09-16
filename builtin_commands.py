c2ba717d205981a680803b2133ceb508ae8b6a97
gkbrk <webdosusb@gmail.com>
2013-09-16 18:01:50 +0300

    Added PM system. (whisper or /msg)

--- builtin_commands.py
+++ builtin_commands.py
@@ -4,28 +4,34 @@ def help_command(player, arguments):
     send = lambda x: player.packetSend.chat(x)
     num_args = len(arguments)
 
-    if num_args:
-        # There are arguments, so display help message.
+    if not num_args:
+        # No arguments, list commands.
         send("pymine2 Help")
         send("============")
         send("Welcome to pymine2, a Python-based MC server.")
         send("")
-        send("To get a list of commands, please use /help.")
+        send("To get a list of commands, please use /help commands.")
     else:
-        # No arguments, list commands.
+        # There are arguments, so display help message.
         send("pymine2 Commands")
         send("================")
         send("/help")
         send("/list")
+        send("/msg targetName message")
 
 def list_command(player, arguments):
     send = lambda x: player.packetSend.chat(x)
     send(str(len(player.server.get_players())) + " players online:")
     send(",".join( [pl.username for pl in player.server.get_players()] ))
 
+def msg_command(player, arguments):
+    message = " ".join(arguments[1:])
+    player.server.find_player(arguments[0]).packetSend.chat(color_codes.yellow + player.username + " whispers to you: " + color_codes.white + message)
+
 command_mapping = {
 "help": help_command,
-"list": list_command
+"list": list_command,
+"msg": msg_command
 }
 
 def run_command(player, command, arguments):
