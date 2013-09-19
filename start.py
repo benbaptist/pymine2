6728ea0db07d1d600e202cbdb6e9fc9e1432e245
gkbrk <webdosusb@gmail.com>
2013-09-19 20:36:24 +0300

    Added Queue to Logger class so race conditions dont cause outputs like "[1379610306.24] [INFO] World tick loop begin[1379610306.24] [INFO] Listening for clients on port 25565" on a single line.

--- start.py
+++ start.py
@@ -1,18 +1,33 @@
-import server, time
-class Log:
+import server, time, Queue, threading
+class PymineLogger:
 	def __init__(self):
 		self.file = open('server.log', 'a')
+		self.abort = False
+		self.log_queue = Queue.Queue()
+		threading.Thread(target=self.process_queue, args=()).start()
+	
 	def error(self, i):
 		line = u'[%s] [ERROR] %s' % (str(time.time()), i)
-		print line
-		self.file.write(u'%s\n' % line)
-		self.file.flush()
+		self.log_queue.put(line)
+	
 	def info(self, i):
 		line = u'[%s] [INFO] %s' % (str(time.time()), i)
-		print line
-		self.file.write(u'%s\n' % line)
-		self.file.flush()
-log = Log()
+		self.log_queue.put(line)
+	
+	def process_queue(self):
+		while not self.abort:
+			try:
+				log_line = self.log_queue.get(timeout=1)
+				print log_line
+				self.file.write("%s\n" % log_line)
+				self.file.flush()
+			except Queue.Empty:
+				pass
+	
+	def stop(self):
+		self.abort = True
+
+log = PymineLogger()
 
 server = server.Server(log)
 server.setup()
@@ -20,4 +35,5 @@ try:
 	server.listen()
 except KeyboardInterrupt:
 	log.info("Gracefully terminating")
+	log.stop()
 	server.close()
