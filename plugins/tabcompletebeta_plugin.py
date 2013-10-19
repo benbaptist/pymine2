d4bcbfd4c7184cd148e186a20b01cc9b453546fc
gkbrk <webdosusb@gmail.com>
2013-10-19 22:33:35 +0300

    Added tab-complete support.

--- plugins/tabcompletebeta_plugin.py
+++ plugins/tabcompletebeta_plugin.py
@@ -1,4 +1,3 @@
-#Not finished yet. I will finish that after my vacation.
 import builtin_commands
 class tabcompletebeta:
     def __init__(self, server):
@@ -8,12 +7,28 @@ class tabcompletebeta:
     def tabcomplete_got(self, event):
         if event.packet["id"] == 0xCB:
             text = event.packet["text"]
-            if text[0] == "/":
+            last_word = text.split()[-1]
+            if last_word[0] == "/":
                 #It's a command.
                 matching_commands = []
                 for commandname in builtin_commands.command_mapping:
-                    if commandname.startswith(text[1:]):
+                    if commandname.startswith(last_word[1:]):
                         matching_commands.append(commandname)
-                if matching_commands.__len__():
+                if matching_commands.__len__() == 1:
                     #There is only 1 command that matches.
-                    event.player.packetSend.tab_complete(matching_commands[0])
+                    event.player.packetSend.tab_complete("/" + matching_commands[0])
+                elif matching_commands.__len__() > 1:
+                    #There are multiple matches.
+                    event.player.packetSend.tab_complete(u"\x00".join(["/" + func_name for func_name in matching_commands]))
+            else:
+                #It's a player name.
+                matching_names = []
+                for player in self.server.get_players():
+                    if player.username.startswith(last_word[1:]):
+                        matching_names.append(player.username)
+                if matching_names.__len__() == 1:
+                    #There is only 1 name that matches.
+                    event.player.packetSend.tab_complete(matching_names[0])
+                elif matching_names.__len__ > 1:
+                    #There are multiple matches.
+                    event.player.packetSend.tab_complete(u"\x00".join(matching_names))
